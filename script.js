42bb605f9c8a176d802ef035581453ec29eae14f
rahulaleshwar <rahulaleshwar@gmail.ocm>
2020-07-27 17:22:53 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function aBoutME() {
+  location.replace("./Aboutme.html")
+}
+function conTa() {
+    location.replace("./contact.html")
+  }
+  function eDu() {
+    location.replace("./Edu.html")
+  }
